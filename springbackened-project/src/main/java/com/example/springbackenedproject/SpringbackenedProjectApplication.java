1e59ec4b4df958bb569c4f77328eef43938768ee
syedshams111111 <shamsjani1234@gmail.com>
2023-05-16 23:19:33 +0500

    init project

---
+++ springbackened-project/src/main/java/com/example/springbackenedproject/SpringbackenedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbackenedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbackenedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbackenedProjectApplication.class, args);
+	}
+
+}
---
+++ springbackened-project/src/test/java/com/example/springbackenedproject/SpringbackenedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbackenedproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbackenedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
